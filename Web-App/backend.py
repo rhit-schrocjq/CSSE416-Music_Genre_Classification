3329c6b973d85b9027d5c27c2d27a1eeba3dff40
ROSE-HULMAN\schrocjq <schrocjq@rose-hulman.edu>
2024-11-19 13:50:45 -0500

    added mixed model to website

--- Web-App/backend.py
+++ Web-App/backend.py
@@ -14,6 +14,8 @@ import torch.nn.functional as F
 import shutil
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
+from panns_inference import AudioTagging
+import librosa
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -37,8 +39,9 @@ class NumpyDataset(Dataset):
         # Load file paths and labels
         if os.path.isdir(dir):
             for file_name in os.listdir(dir):
-                file_path = os.path.join(dir, file_name)
-                self.Zsamples.append(file_path)
+                if file_name.endswith("Z.npy"):
+                    file_path = os.path.join(dir, file_name)
+                    self.Zsamples.append(file_path)
 
     
     def __len__(self):
@@ -61,6 +64,41 @@ class NumpyDataset(Dataset):
         if self.transform:
             data = self.transform(data)
         
+        return data
+    
+# Custom Instrment Dataset class
+class InstrumentDataset(Dataset):
+    def __init__(self, dir):
+        self.dir = dir
+        self.Isamples = []
+        
+        # Load file paths and labels
+        if os.path.isdir(dir):
+            for file_name in os.listdir(dir):
+                if file_name.endswith("I.npy"):
+                        file_path = os.path.join(dir, file_name)
+                        self.Isamples.append(file_path)
+    
+    def __len__(self):
+        return len(self.Isamples)
+    
+    def __getitem__(self, idx):
+        file_path = self.Isamples[idx]
+        data = np.load(file_path)  # Load numpy array
+        data = torch.tensor(data, dtype=torch.float32)  # Convert to PyTorch tensor
+        
+        return data
+    
+class SimpleDataset(Dataset):
+    def __init__(self, X):
+        self.X = X
+    
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        data = self.X[idx]
+        data = torch.tensor(data, dtype=torch.float32)  # Convert to PyTorch tensor
         return data
 
 
@@ -121,6 +159,22 @@ class MusicNet(nn.Module):
             x = self.fLayer(x)
              
         return x
+    
+class FFNet(nn.Module):
+    def __init__(self):
+        super(FFNet, self).__init__()
+
+        self.fLayer = nn.Sequential(
+            nn.Linear(in_features=263168, out_features=1024),
+            nn.ReLU(),
+            nn.Linear(in_features=1024, out_features=128),
+            nn.ReLU(),
+            nn.Linear(in_features=128, out_features=10)
+        )
+
+    def forward(self, x):
+        x = self.fLayer(x)   
+        return x
 
 # Function to convert mp3 to wav if necessary
 def convert_mp3_to_wav(input_file: str, output_file: str, sample_rate: int = 22050):
@@ -134,32 +188,45 @@ def convert_mp3_to_wav(input_file: str, output_file: str, sample_rate: int = 220
         print(f"Error during conversion: {e}")
 
 # Function to convert audio to spectrograms (sample_rate=22050, clip_length=6*22050+1=132301)
-def audio_to_spectrograms(filepath, savepath, sample_rate=22050, clip_length=132301):
+def audio_to_spectrograms(filepath, savepath, sample_rate=22050, music_clip_length=132301, instrument_clip_length=192001):
     # Ensure file is in .wav format
     # filepath = convert_to_wav(filepath, sample_rate)
     
     # Read audio file
     print("Going to read the file path: " + filepath)
-    sr, samples = wavfile.read(filepath)
-    if sr != sample_rate:
-        raise ValueError(f"Expected sample rate {sample_rate}, but got {sr}")
-
+    #spectrogram sampling
+    music_sr, music_samples = wavfile.read(filepath)
+    if music_sr != sample_rate:
+        raise ValueError(f"Expected sample rate {sample_rate}, but got {music_sr}")
     # Mono conversion
-    if len(samples.shape) > 1:
-        samples = np.mean(samples, axis=1)
+    if len(music_samples.shape) > 1:
+        music_samples = np.mean(music_samples, axis=1)
 
-    # create 6sec samples
-    clip_samples=[]
+    #insturment sampling
+    instrument_samples, instrument_sr = librosa.load(filepath, sr=32000, mono=True)
+
+    # music clip samples
+    music_clip_samples=[]
     start = 0
-    end = start + clip_length
+    end = start + music_clip_length
 
-    while (end < len(samples)):
-        clip_samples.append(samples[start: end])
+    while (end < len(music_samples)):
+        music_clip_samples.append(music_samples[start: end])
         start = end
-        end = end + clip_length
+        end = end + music_clip_length
+
+    # insturment clip samples    
+    instrument_clip_samples = []
+    start = 0
+    end = start + instrument_clip_length
+    
+    while (end < len(instrument_samples)):
+        instrument_clip_samples.append(instrument_samples[start: end])
+        start = end
+        end = end + instrument_clip_length
 
     i=0
-    for sample in clip_samples:
+    for sample in music_clip_samples:
         SFT = signal.ShortTimeFFT.from_window(win_param='tukey', 
                                             fs=sample_rate, 
                                             nperseg=sample_rate//20,      #make 20Hz minimum sampled frequency
@@ -169,8 +236,75 @@ def audio_to_spectrograms(filepath, savepath, sample_rate=22050, clip_length=132
                                             phase_shift=None,
                                             symmetric_win=True)
         Zxx = SFT.stft(sample)
+        Ix = instrument_clip_samples[i]
         np.save(savepath + "sample_" + str(i) + "_Z.npy", Zxx)
+        np.save(savepath + "sample_" + str(i) + "_I.npy", Ix)
         i+=1
+
+def music_features(model, state_dict, loader, save_path):
+    # Load model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    model.load_state_dict(torch.load(state_dict, weights_only=False, map_location=device))  # Use map_location for device compatibility
+    model.eval()  # Set model to evaluation mode
+    # Dictionary to store features and labels
+    features = []
+
+    # Extract features
+    with torch.no_grad():  # We don't need gradients for this task
+        for images in loader:
+            images = images.to(device)
+            
+            # Extract features from the images
+            # The VGG16 model outputs feature maps, which we can treat as image features
+            outputs = model(images, return_features=True)
+            
+            # Flatten the outputs to a 1D vector
+            outputs = outputs.view(outputs.size(0), -1)
+            
+            # Append features and labels to lists
+            features.append(outputs.cpu().numpy())
+
+    print("Model Done")
+    
+    features = np.concatenate(features, axis=0)
+    np.savez(save_path, features=features)
+    
+    print("Features saved")
+
+def instrument_features(loader, save_path):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Using device: {device}')
+    audio_tagging = AudioTagging(checkpoint_path=None, device=device)
+    # Dictionary to store features and labels
+    features = []
+
+    # Extract features
+    for audio in loader:
+       
+        # Extract features for the batch
+        _, embedding = audio_tagging.inference(audio)
+    
+        # Flatten the outputs to a 1D vector
+        outputs = np.array(embedding.flatten())
+        
+        # Append features and labels to lists
+        features.append(outputs)
+    
+    print("Model Done")
+    
+    np.savez(save_path, features=features)
+    
+    print("Features saved")
+
+def concatenate_features(npz1, npz2):
+    data1 = np.load(npz1)
+    data2 = np.load(npz2)
+
+    features = np.concatenate((data1['features'], data2['features']), axis=1)
+    print(features.shape)
+
+    return features
 
 
 # Testing loop
@@ -240,12 +374,25 @@ def classify_genre():
     audio_to_spectrograms(file_path, app.config['NPY_FOLDER'])
     print("Back from creating the spectogram and going to perform prediction")
 
+    #generate music features
+    print("Creating Music Features")
     music_dataset = NumpyDataset(dir=app.config['NPY_FOLDER'], transform=transform)
     music_loader = DataLoader(dataset=music_dataset, batch_size=1, shuffle=False)
-    
-    # Perform prediction
+    music_features(MusicNet(), "MusicGenreClassifier.pth", music_loader, os.path.join(app.config['NPY_FOLDER'], 'music_features.npz'))
+
+    #generate instrument features
+    print("Creating Instrument Features")
+    instrument_dataset = InstrumentDataset(dir=app.config['NPY_FOLDER'])
+    instrument_loader = DataLoader(dataset=instrument_dataset, batch_size=1, shuffle=False)
+    instrument_features(instrument_loader, os.path.join(app.config['NPY_FOLDER'], 'instrument_features.npz'))
+
+    # Perform prediction on combined features
+    print("Performing Classifications")
+    features = concatenate_features(os.path.join(app.config['NPY_FOLDER'], 'music_features.npz'), os.path.join(app.config['NPY_FOLDER'], 'instrument_features.npz'))
+    combined_dataset = SimpleDataset(features)
+    combined_loader = DataLoader(dataset=combined_dataset, batch_size=1, shuffle=False)
     classes = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
-    predictions = music_classifier(MusicNet(), "MusicGenreClassifier.pth", music_loader, classes)
+    predictions = music_classifier(FFNet(), "MixedMusicGenreClassifier.pth", combined_loader, classes)
 
     # Clean up uploaded file
     os.remove(file_path)
